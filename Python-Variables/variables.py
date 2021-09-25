e12b7475b9ba09e5206a48ba8592214838f6432e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-25 15:59:03 -0400

    added examples for datatypes in python

--- Python-Variables/variables.py
+++ Python-Variables/variables.py
@@ -1,1 +1,62 @@
 # Python variables - Data types - Basic Grammar
+
+
+# 1 - Variables names must make sense
+# 2 - Python covention is to use lowercase letters
+# 3 - Python is snake case _
+# 4 - Variables names must start with a letter or _
+# 5 - Following the pep8 standards
+
+# --- Strings ---
+my_favorite_game = "Skyrim"
+my_first_name = "Daniel"
+
+
+# --- Numbers ---
+
+my_age = 31
+my_score = 92
+
+# --- Boolean ---
+im_canadian = True
+im_female = False
+
+
+"""
+--Data Types              --Class--    --Value--
+integers                  -> int         -> 45
+floating point numbers    -> float       -> 4.5
+booleans                  -> bool        -> True/False
+strings                   -> str         -> "Daniel Ducas-Viramontes"
+list                      -> list        -> [1,2,3]
+dictionary                -> dict        -> {"user_name": "awesome50", "user_id":56}
+tuple                     -> tuple       -> (10,20,30)
+set                       -> set         -> {"cat", 99}
+
+"""
+
+
+# integer-------------------------------------------------------
+age = 31
+print(age)
+print(type(age))
+
+
+# floats--------------------------------------------------------
+grade = 9.1
+print(grade)
+print(type(grade))
+
+# boolans------------------------------------------------------
+alarm = True
+offline = False
+print(alarm, offline)
+print(type(alarm))
+print(type(offline))
+
+
+# strings ----------------------------------------------------
+
+movie_name = "The matrix"
+print(movie_name)
+print(type(movie_name))
