e437fb320c152d30dcda58dba16771fc4a3e4c7e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-26 10:51:52 -0400

    added number_methods and operations

---
+++ Python-Variables-DataTypes-BasicGrammar/number_methods_operations.py
@@ -0,0 +1,41 @@
+
+# ---------------------------------------------------
+# Number Methods & Operations
+
+import math
+
+# Arithmetic Operations
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(5 / 5)
+print(25 // 5)
+print(50 % 11)
+print(2 ** 2)
+
+print("----------------------------------------")
+
+# Augmented Assignment operator
+number = 10
+# number = number + 5
+number += 5
+print(number)
+
+
+# Number Methods
+
+
+# 1 - round()
+
+print(round(3.1))
+print(round(5.24))
+
+
+# 2 - abs()
+print(abs(-5.21))
+
+
+# link -> https://docs.python.org/3/library/math.html
+
+
+# print(math.) -> many math methods look up the documentation
