f9683232e47e16fefd726a01ce5341fd7647f06e
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-26 11:09:14 -0400

    added examples for type_conversion

---
+++ Python-Variables-DataTypes-BasicGrammar/type_conversion.py
@@ -0,0 +1,20 @@
+
+
+# ------ Type conversion -----
+
+num = int(input("Number: "))
+print(type(num))
+
+# int()
+# float()
+# str()
+# bool()
+
+
+# falsy values
+# 0
+# ""
+# None
+print(bool(0))
+print(bool(""))
+print(bool(None))
