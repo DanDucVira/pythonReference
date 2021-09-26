7e6dd75da61ced4b1179c749efcec3f080d797d1
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-26 12:57:52 -0400

    added more examples

--- Python-Variables-DataTypes-BasicGrammar/namespace.py
+++ Python-Variables-DataTypes-BasicGrammar/namespace.py
@@ -6,3 +6,19 @@
 # Build-in Namespace
 # Module: Global Namespace
 # Function:Local Namespace
+
+
+def outer():
+    outer_number = 100
+
+    def inner():
+        inner_number = 200
+        print("Inner number =", inner_number)
+
+    inner()
+
+
+global_number = 300
+
+
+outer()
