71560796f183fd28e61e4c6efcea6879b0bcdfb0
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-26 13:07:28 -0400

    added file input_output.py

---
+++ Python-Variables-DataTypes-BasicGrammar/input_output.py
@@ -0,0 +1,21 @@
+
+
+# ------ Input  & output ------
+
+# Example ->>>>>> Output
+print(" This is a string")
+print(24)
+print(True)
+print([1, 2, 3])
+first_name = "Daniel"
+last_name = "Ducas-Viramontes"
+
+print("My first name is {} and my last name is {}, so my full name is {} {}".format(
+    first_name, last_name, first_name, last_name))
+
+
+
+
+
+
+
