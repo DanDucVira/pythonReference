5cb1f11afacaf08601ad526fcb827c36e62fd024
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-09-26 08:49:00 -0400

    added escape_sequences examples

---
+++ Python-Variables-DataTypes-BasicGrammar/escape_sequences.py
@@ -0,0 +1,20 @@
+
+
+# ------ #Escape Sequences#------
+
+
+# \'
+# \"
+# \\
+# \n
+
+
+#story_desc = "It's a touching story"
+
+#story_desc = "It\"s a touching story"
+
+#story_desc = 'It\'s a touching story'
+
+story_desc = "It\\'s a touching story"
+
+print(story_desc)
